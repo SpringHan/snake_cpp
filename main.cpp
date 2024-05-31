a28dadadaee3d51890c3f3dd2e028116def60246
springhan <3379184879@qq.com>
2024-05-31 14:34:17 +0800

    Import typical qt files into project

--- main.cpp
+++ main.cpp
@@ -1,17 +1,11 @@
-#include "qlabel.h"
-#include "./src/game.hpp"
-
-#include <iostream>
+#include "mainwindow.hpp"
 
 #include <QApplication>
-#include <QLabel>
 
-int main(int argc, char **argv) {
-	QApplication app(argc, argv);
-	QLabel hello("Test");
-
-	hello.show();
-	return app.exec();
-
-	// return 0;
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
--- src/main_window.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef _MAIN_WINDOW_H_
-#define _MAIN_WINDOW_H_
-
-#include <QMainWindow>
-
-#endif
