6e04e4cf3c3ebce0bdb7dd8d3c9f4c82f3f29fc9
springhan <3379184879@qq.com>
2024-05-31 19:32:50 +0800

    Modified the name of executable file, added shell file for running

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,19 @@
 #include "mainwindow.hpp"
+#include "qlabel.h"
+#include "qobjectdefs.h"
+#include "qpushbutton.h"
+
+#include <QPushButton>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-{}
+	: QMainWindow(parent)
+{
+	button = new QPushButton("Text", this);
+	temp_text = new QLabel("Text", this);
+	temp_text->setGeometry(200, 200, 100, 30);
+	// button->setGeometry(10, 10, 80, 30);
+
+	// QObject::connect(button, SIGNAL(click()), );
+}
 
 MainWindow::~MainWindow() {}
--- mainwindow.hpp
+++ mainwindow.hpp
@@ -1,14 +1,23 @@
 #ifndef MAINWINDOW_HPP
 #define MAINWINDOW_HPP
 
+#include "qlabel.h"
+#include "qpushbutton.h"
 #include <QMainWindow>
+#include <QLabel>
+#include <QPushButton>
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+	MainWindow(QWidget *parent = nullptr);
+	~MainWindow();
+
+private:
+	QPushButton *button;
+	QLabel *temp_text; 
 };
+
 #endif // MAINWINDOW_HPP
