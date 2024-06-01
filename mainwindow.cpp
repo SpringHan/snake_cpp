ae26658e9b6ee32d4db1dca646f495b360105da5
springhan <3379184879@qq.com>
2024-06-01 20:14:31 +0800

    Finished basic layout

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,19 +1,38 @@
 #include "mainwindow.hpp"
-#include "qlabel.h"
+#include "qboxlayout.h"
+#include "qnamespace.h"
 #include "qobjectdefs.h"
 #include "qpushbutton.h"
+#include "qtableview.h"
 
 #include <QPushButton>
+#include <QTableView>
+#include <QBoxLayout>
 
 MainWindow::MainWindow(QWidget *parent)
 	: QMainWindow(parent)
 {
-	button = new QPushButton("Text", this);
-	temp_text = new QLabel("Text", this);
-	temp_text->setGeometry(200, 200, 100, 30);
-	// button->setGeometry(10, 10, 80, 30);
+	this->setWindowTitle("Snake game");
 
-	// QObject::connect(button, SIGNAL(click()), );
+	// Controller layout
+	start_btn = new QPushButton("开始");
+	pause_btn = new QPushButton("暂停");
+
+	control_line = new QHBoxLayout;
+	control_line->addWidget(start_btn, 0, Qt::Alignment().setFlag(Qt::AlignLeft));
+	control_line->addWidget(pause_btn, 1, Qt::Alignment().setFlag(Qt::AlignRight));
+
+	// Game blocks
+	blocks_table = new QTableView;
+
+	// Whole Vertical layout
+	whole = new QVBoxLayout;
+	whole->addLayout(control_line);
+	whole->addWidget(blocks_table);
+
+	main_widget = new QWidget(this);
+	main_widget->setLayout(whole);
+	this->setCentralWidget(main_widget);
 }
 
 MainWindow::~MainWindow() {}
--- mainwindow.hpp
+++ mainwindow.hpp
@@ -1,11 +1,17 @@
 #ifndef MAINWINDOW_HPP
 #define MAINWINDOW_HPP
 
+#include "qboxlayout.h"
 #include "qlabel.h"
+#include "qobjectdefs.h"
 #include "qpushbutton.h"
+#include "qtableview.h"
 #include <QMainWindow>
+
 #include <QLabel>
 #include <QPushButton>
+#include <QTableView>
+#include <QBoxLayout>
 
 class MainWindow : public QMainWindow
 {
@@ -16,8 +22,15 @@ public:
 	~MainWindow();
 
 private:
-	QPushButton *button;
-	QLabel *temp_text; 
+	QWidget *main_widget;
+	QVBoxLayout *whole;
+	QHBoxLayout *control_line;
+	QPushButton *start_btn;
+	QPushButton *pause_btn;
+	QTableView *blocks_table;
+
+// private slots:
+// 	void changeText();
 };
 
 #endif // MAINWINDOW_HPP
